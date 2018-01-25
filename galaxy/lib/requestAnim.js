1633329e188311806fd5600b636b7e1ca3498238
ARM9 <toast.dslua@gmail.com>
2018-01-25 17:08:07 +0100

    No copyright intendo

---
+++ galaxy/lib/requestAnim.js
@@ -0,0 +1,2 @@
+// requestAnim shim layer by Paul Irish
+window.requestAnimFrame=(function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(callback,element){window.setTimeout(callback,1000/60);};})();
